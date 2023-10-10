61818f6d8687fa4c14da56b526f743c167e3467d
HoangLam21 <hoanglamdeveloper@gmail.com>
2023-10-10 08:10:18 +0700

    CMToMerge

--- Lab1/1.4/1.4/Program.cs
+++ Lab1/1.4/1.4/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+<<<<<<< HEAD
 namespace _1._4
 {
     internal class Program
@@ -11,6 +12,70 @@ namespace _1._4
         static void Main(string[] args)
         {
             //ghjiko
+=======
+namespace temperature
+{
+    public delegate void ThayDoiNhietDo(float NhietDo);
+    public class NhietKe
+    {
+        public event ThayDoiNhietDo NhietDoThayDoi;
+        private float nhietdo;
+
+        public float NhietDo
+        {
+            get { return this.nhietdo; }
+            set { this.nhietdo = value; }
+        }
+        public NhietKe(float NhietDoBanDau = 0)
+        {
+            this.nhietdo = NhietDoBanDau;
+        }
+        public void updateNhietDo()
+        {
+            Random randomnd = new Random();
+            float nhietdomoi = (float)randomnd.NextDouble() * 100;
+            if (nhietdomoi != this.nhietdo)
+            {
+                this.nhietdo = nhietdomoi;
+                NhietDoThayDoi?.Invoke(nhietdomoi);
+
+            }
+
+        }
+    }
+    class ManHinhHienThiNhietDo
+    {
+        public void DKNhanThongBaoSuKienThayDoiNhietDo(NhietKe nhietke)
+        {
+            nhietke.NhietDoThayDoi += Nhietke_NhietDoThayDoi;
+        }
+        public void HuyDKNhanThongBaoSuKienThayDoiNhietDo(NhietKe nhietke)
+        {
+            nhietke.NhietDoThayDoi -= Nhietke_NhietDoThayDoi;
+        }
+        private void Nhietke_NhietDoThayDoi(float NhietDo)
+        {
+            Console.WriteLine($"Nhiet do moi la: {NhietDo} do C ");
+        }
+    }
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            NhietKe nhietke = new NhietKe();
+            Console.WriteLine($"Nhiet do ban dau la: {nhietke.NhietDo} do C");
+            ManHinhHienThiNhietDo manhinhhienthinhietdo = new ManHinhHienThiNhietDo();
+            manhinhhienthinhietdo.DKNhanThongBaoSuKienThayDoiNhietDo(nhietke);
+            do
+            {
+                nhietke.updateNhietDo();
+                Console.WriteLine("nhan Enter de tiep tuc, nhan 0 de ket thuc ");
+            } while (Console.ReadLine() != "0");
+            manhinhhienthinhietdo.HuyDKNhanThongBaoSuKienThayDoiNhietDo(nhietke);
+            //Console.ReadLine();
+>>>>>>> NhuHuynh
         }
     }
 }
