9d9e8030f098d69fdc7272cbbc18dba8bb8bb9d1
GiaBaoAnh04 <146148975+GiaBaoAnh04@users.noreply.github.com>
2023-10-10 11:01:27 +0700

    1.6 update

---
+++ 1.6/Bai1/Program.cs
@@ -0,0 +1,153 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_01
+{
+    internal class Program
+    {
+
+
+        class MaxInt
+        {
+            private int[] arr;
+
+            public void Nhap()
+            {
+                Console.WriteLine("Nhap so luong phan tu so nguyen: ");
+                int n = int.Parse(Console.ReadLine());
+                arr = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("Nhap vao gia tri phan tu thu {0}: ", i + 1);
+                    arr[i] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            public int TimMax()
+            {
+
+                int max = arr[0];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] > max)
+                    {
+                        max = arr[i];
+                    }
+                }
+                return max;
+
+            }
+        }
+
+        class MaxFloat
+        {
+            private float[] arr;
+
+            public void Nhap()
+            {
+                Console.WriteLine("Nhap so luong phan tu so thuc: ");
+                int n = int.Parse(Console.ReadLine());
+                arr = new float[n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("Nhap vao gia tri phan tu thu {0}: ", i + 1);
+                    arr[i] = float.Parse(Console.ReadLine());
+                }
+            }
+
+            public float TimMax()
+            {
+                float max = arr[0];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] > max)
+                    {
+                        max = arr[i];
+                    }
+                }
+                return max;
+
+            }
+        }
+
+        class MaxSTring
+        {
+            private string[] arr;
+
+            public void Nhap()
+            {
+                Console.WriteLine("Nhap so luong phan tu chuoi: ");
+                int n = int.Parse(Console.ReadLine());
+                arr = new string[n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("Nhap vao gia tri phan tu thu {0}: ", i + 1);
+                    arr[i] = Console.ReadLine();
+                }
+            }
+
+            public string TimMax()
+            {
+                string max = arr[0];
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i].Length > max.Length)
+                    {
+                        max = arr[i];
+                    }
+                }
+                return max;
+
+            }
+        }
+
+        class MaxChar
+        {
+            private char[] arr;
+
+            public void Nhap()
+            {
+                Console.WriteLine("Nhap so luong phan tu char: ");
+                int n = int.Parse(Console.ReadLine());
+                arr = new char[n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("Nhap vao gia tri phan tu thu {0}: ", i + 1);
+                    arr[i] = char.Parse(Console.ReadLine());
+                }
+            }
+
+            
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            MaxInt maxInt = new MaxInt();
+            maxInt.Nhap();
+            int MaxSN = maxInt.TimMax();
+
+            MaxFloat maxFloat = new MaxFloat();
+            maxFloat.Nhap();
+            float MaxST = maxFloat.TimMax();
+
+            MaxSTring maxString = new MaxSTring();
+            maxString.Nhap();
+            string MaxS = maxString.TimMax();
+
+            Console.WriteLine("So lon nhat trong 1 day so nguyen: " + MaxSN + "\r\n");
+            Console.WriteLine("So lon nhat trong 1 day so thuc: " + MaxST + "\r\n");
+            Console.WriteLine("Chuoi lon nhat trong 1 day chuoi: " + MaxS + "\r\n");
+
+        }
+
+    }
+}
