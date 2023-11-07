7b67638fb8309a13c8eb4fa6e337bb2fa0a65be6
GiaBaoAnh04 <146148975+GiaBaoAnh04@users.noreply.github.com>
2023-11-07 22:33:15 +0700

    menustrip

---
+++ Lab02/2.5/2.5/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace _2._5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            editToolStripMenuItem = new ToolStripMenuItem();
+            viewsToolStripMenuItem = new ToolStripMenuItem();
+            helpToolStripMenuItem = new ToolStripMenuItem();
+            renameToolStripMenuItem = new ToolStripMenuItem();
+            deleteToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            copyToolStripMenuItem = new ToolStripMenuItem();
+            cutToolStripMenuItem = new ToolStripMenuItem();
+            pasteToolStripMenuItem = new ToolStripMenuItem();
+            largeIconsToolStripMenuItem = new ToolStripMenuItem();
+            smallIconsToolStripMenuItem = new ToolStripMenuItem();
+            listIconsToolStripMenuItem = new ToolStripMenuItem();
+            detailsToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, editToolStripMenuItem, viewsToolStripMenuItem, helpToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { renameToolStripMenuItem, deleteToolStripMenuItem, exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(46, 24);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // editToolStripMenuItem
+            // 
+            editToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { copyToolStripMenuItem, cutToolStripMenuItem, pasteToolStripMenuItem });
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new Size(49, 24);
+            editToolStripMenuItem.Text = "Edit";
+            // 
+            // viewsToolStripMenuItem
+            // 
+            viewsToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { largeIconsToolStripMenuItem, smallIconsToolStripMenuItem, listIconsToolStripMenuItem, detailsToolStripMenuItem });
+            viewsToolStripMenuItem.Name = "viewsToolStripMenuItem";
+            viewsToolStripMenuItem.Size = new Size(61, 24);
+            viewsToolStripMenuItem.Text = "Views";
+            // 
+            // helpToolStripMenuItem
+            // 
+            helpToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { aboutToolStripMenuItem });
+            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            helpToolStripMenuItem.Size = new Size(55, 24);
+            helpToolStripMenuItem.Text = "Help";
+            // 
+            // renameToolStripMenuItem
+            // 
+            renameToolStripMenuItem.Name = "renameToolStripMenuItem";
+            renameToolStripMenuItem.ShortcutKeys = Keys.F2;
+            renameToolStripMenuItem.Size = new Size(224, 26);
+            renameToolStripMenuItem.Text = "Rename ";
+            renameToolStripMenuItem.Click += renameToolStripMenuItem_Click;
+            // 
+            // deleteToolStripMenuItem
+            // 
+            deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            deleteToolStripMenuItem.ShortcutKeys = Keys.Delete;
+            deleteToolStripMenuItem.Size = new Size(224, 26);
+            deleteToolStripMenuItem.Text = "Delete";
+            deleteToolStripMenuItem.Click += deleteToolStripMenuItem_Click;
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.W;
+            exitToolStripMenuItem.Size = new Size(224, 26);
+            exitToolStripMenuItem.Text = "Exit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // copyToolStripMenuItem
+            // 
+            copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            copyToolStripMenuItem.Size = new Size(224, 26);
+            copyToolStripMenuItem.Text = "Copy";
+            copyToolStripMenuItem.Click += copyToolStripMenuItem_Click;
+            // 
+            // cutToolStripMenuItem
+            // 
+            cutToolStripMenuItem.Name = "cutToolStripMenuItem";
+            cutToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.X;
+            cutToolStripMenuItem.Size = new Size(224, 26);
+            cutToolStripMenuItem.Text = "Cut";
+            cutToolStripMenuItem.Click += cutToolStripMenuItem_Click;
+            // 
+            // pasteToolStripMenuItem
+            // 
+            pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            pasteToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.V;
+            pasteToolStripMenuItem.Size = new Size(224, 26);
+            pasteToolStripMenuItem.Text = "Paste";
+            pasteToolStripMenuItem.Click += pasteToolStripMenuItem_Click;
+            // 
+            // largeIconsToolStripMenuItem
+            // 
+            largeIconsToolStripMenuItem.Name = "largeIconsToolStripMenuItem";
+            largeIconsToolStripMenuItem.Size = new Size(224, 26);
+            largeIconsToolStripMenuItem.Text = "Large icons";
+            largeIconsToolStripMenuItem.Click += largeIconsToolStripMenuItem_Click;
+            // 
+            // smallIconsToolStripMenuItem
+            // 
+            smallIconsToolStripMenuItem.Name = "smallIconsToolStripMenuItem";
+            smallIconsToolStripMenuItem.Size = new Size(224, 26);
+            smallIconsToolStripMenuItem.Text = "Small icons";
+            smallIconsToolStripMenuItem.Click += smallIconsToolStripMenuItem_Click;
+            // 
+            // listIconsToolStripMenuItem
+            // 
+            listIconsToolStripMenuItem.Name = "listIconsToolStripMenuItem";
+            listIconsToolStripMenuItem.Size = new Size(224, 26);
+            listIconsToolStripMenuItem.Text = "List icons";
+            listIconsToolStripMenuItem.Click += listIconsToolStripMenuItem_Click;
+            // 
+            // detailsToolStripMenuItem
+            // 
+            detailsToolStripMenuItem.Name = "detailsToolStripMenuItem";
+            detailsToolStripMenuItem.Size = new Size(224, 26);
+            detailsToolStripMenuItem.Text = "Details";
+            detailsToolStripMenuItem.Click += detailsToolStripMenuItem_Click;
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(224, 26);
+            aboutToolStripMenuItem.Text = "About";
+            aboutToolStripMenuItem.Click += aboutToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem editToolStripMenuItem;
+        private ToolStripMenuItem viewsToolStripMenuItem;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private ToolStripMenuItem renameToolStripMenuItem;
+        private ToolStripMenuItem deleteToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem copyToolStripMenuItem;
+        private ToolStripMenuItem cutToolStripMenuItem;
+        private ToolStripMenuItem pasteToolStripMenuItem;
+        private ToolStripMenuItem largeIconsToolStripMenuItem;
+        private ToolStripMenuItem smallIconsToolStripMenuItem;
+        private ToolStripMenuItem listIconsToolStripMenuItem;
+        private ToolStripMenuItem detailsToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
---
+++ Lab02/2.5/2.5/Form1.cs
@@ -0,0 +1,256 @@
+﻿namespace _2._5
+{
+    public partial class Form1 : Form
+    {
+
+        private bool isCutOperation;
+        private ListViewItem clipboardItm;
+        private FileSystemInfo clipboardItem;
+        private object detailsToolStripMenuItem;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void renameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+                string currentName = selectedItem.Text;
+
+                // Hiển thị hộp thoại đổi tên (hoặc sử dụng TextBox trực tiếp trên ListViewItem)
+                string newName = Microsoft.VisualBasic.Interaction.InputBox("Enter new name:", "Rename", currentName);
+
+                if (!string.IsNullOrEmpty(newName))
+                {
+                    selectedItem.Text = newName;
+                }
+            }
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                ListViewItem selectedItem = listView1.SelectedItems[0];
+
+                // Hiển thị hộp thoại xác nhận
+                DialogResult result = MessageBox.Show($"Are you sure you want to delete {selectedItem.Text}?", "Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if (result == DialogResult.Yes)
+                {
+                    // Thực hiện hành động xóa
+                    listView1.Items.Remove(selectedItem);
+                }
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dlr = MessageBox.Show("Do you want to exit ?", "Message", MessageBoxButtons.YesNo);
+            if (dlr == DialogResult.Yes) Application.Exit();
+        }
+
+
+        public class FileSystemInfoItem
+        {
+            public FileSystemInfo FileSystemInfo { get; set; }
+
+            public FileSystemInfoItem(FileSystemInfo info)
+            {
+                FileSystemInfo = info;
+            }
+
+            public static implicit operator FileSystemInfo(FileSystemInfoItem v)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private void CopySelectedFolder()
+        {
+            if (treeView1.SelectedNode != null && treeView1.SelectedNode.Tag is DirectoryInfo)
+            {
+                DirectoryInfo selectedFolder = (DirectoryInfo)treeView1.SelectedNode.Tag;
+                clipboardItem = new FileSystemInfoItem(selectedFolder);
+                isCutOperation = false;
+            }
+        }
+
+        private void CopySelectedItem()
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                // Lưu trữ thông tin của item được chọn vào Clipboard
+                clipboardItm = listView1.SelectedItems[0].Clone() as ListViewItem;
+                isCutOperation = false;
+            }
+            else if (treeView1.SelectedNode != null && treeView1.SelectedNode.Tag is DirectoryInfo)
+            {
+                CopySelectedFolder();
+            }
+        }
+
+        private void CutSelectedItem()
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                // Lưu trữ thông tin của item được chọn vào Clipboard và thiết lập cờ cắt
+                clipboardItm = (ListViewItem)listView1.SelectedItems[0].Clone();
+                isCutOperation = true;
+
+                // Xóa item nếu là thao tác cắt
+                listView1.Items.Remove(listView1.SelectedItems[0]);
+            }
+            else if (treeView1.SelectedNode != null && treeView1.SelectedNode.Tag is DirectoryInfo)
+            {
+                CopySelectedFolder();
+            }
+        }
+
+        private void CopyDirectory(string fullName, string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DisplayList(DirectoryInfo directory)
+        {
+            // Hiển thị danh sách thư mục và tập tin trong ListView
+            listView1.Items.Clear();
+            AddFolderListView(directory.GetDirectories());
+            AddFileListView(directory.GetFiles());
+        }
+
+        private void LoadDrive()
+        {
+            treeView1.Nodes.Clear();
+
+            string[] drives = Directory.GetLogicalDrives();
+            TreeNode node = null;
+            foreach (string drive in drives)
+            {
+                node = new TreeNode(drive);
+                node.ImageIndex = 1;
+                treeView1.Nodes.Add(node);
+                node.Nodes.Add("Temp");
+            }
+        }
+        private void PasteSelectedItem()
+        {
+            if (clipboardItem != null)
+            {
+                try
+                {
+                    FileSystemInfoItem fileInfoItem = clipboardItm.Tag as FileSystemInfoItem;
+
+                    if (fileInfoItem != null)
+                    {
+                        // Đối tượng FileSystemInfo chứa thông tin về thư mục hoặc tập tin
+                        FileSystemInfo fileInfo = fileInfoItem.FileSystemInfo;
+
+                        // Đường dẫn thư mục đích (thay thế bằng đường dẫn thư mục bạn muốn paste)
+                        string destinationPath = treeView1.SelectedNode.FullPath;
+
+                        if (fileInfo is DirectoryInfo)
+                        {
+                            // Nếu là thư mục, thực hiện paste thư mục
+                            DirectoryInfo sourceDir = (DirectoryInfo)fileInfo;
+                            DirectoryInfo destinationDir = new DirectoryInfo(Path.Combine(destinationPath, sourceDir.Name));
+
+                            CopyDirectory(sourceDir.FullName, destinationDir.FullName);
+                        }
+                        else if (fileInfo is FileInfo)
+                        {
+                            // Nếu là tập tin, thực hiện paste tập tin
+                            FileInfo sourceFile = (FileInfo)fileInfo;
+                            string destinationFilePath = Path.Combine(destinationPath, sourceFile.Name);
+
+                            File.Copy(sourceFile.FullName, destinationFilePath, true);
+                        }
+
+                        // Nếu là thao tác cắt, không cần làm gì thêm vì đã xóa item gốc khi cut
+
+                        // Refresh ListView
+                        LoadDrive();
+                        DisplayList((DirectoryInfo)treeView1.SelectedNode.Tag);
+
+                        // Xóa thông tin trong Clipboard để tránh paste lặp lại
+                        clipboardItem = null;
+                        clipboardItm = null;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CopySelectedItem();
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CutSelectedItem();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PasteSelectedItem();
+        }
+
+        private void largeIconsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SetView(View.LargeIcon);
+        }
+
+        private void smallIconsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SetView(View.SmallIcon);
+        }
+
+        private void listIconsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SetView(View.List);
+        }
+
+        private void detailsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SetView(View.Details);
+        }
+
+        private void SetView(View view)
+        {
+            listView1.View = view;
+
+            // Tùy chỉnh các thiết lập khác dựa trên chế độ xem nếu cần
+            if (view == View.Details)
+            {
+                // Hiển thị các cột thông tin trong chế độ Details
+                listView1.Columns.Clear();
+                listView1.Columns.Add("Name", 200);
+                listView1.Columns.Add("Type", 100);
+                listView1.Columns.Add("Size", 100);
+                listView1.Columns.Add("Last Modified", 150);
+                listView1.Columns.Add("Path", 400);
+            }
+            else
+            {
+                // Ẩn các cột nếu không phải chế độ Details
+                listView1.Columns.Clear();
+            }
+        }
+
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("co gi hoi lien he hoi ban dao");
+        }
+    }
+}
