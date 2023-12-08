ac4dc623b17ff6dadf23b28268da6c51807b63f1
22520592 <22520592@gm.uit.edu.vn>
2023-12-08 21:13:07 +0700

    UI:Demo&Mainwindow&TimerEvent

---
+++ MINI PROJECT/Mini Project/Mini Project/DemoEvent.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Mini_Project
+{
+    /// <summary>
+    /// Interaction logic for DemoEvent.xaml
+    /// </summary>
+    public partial class DemoEvent : Window
+    {
+        public DemoEvent()
+        {
+            InitializeComponent();
+        }
+
+        private void HenGioButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DangNhap_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void checkBox2_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
